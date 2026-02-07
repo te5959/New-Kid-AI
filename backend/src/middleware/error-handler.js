85775bd1f3f16b51a527ae9b7eb47505cef21ac3
te5959 <konwtf@gmail.com>
2026-02-07 16:18:23 +0100

    Delete backend directory

--- backend/src/middleware/error-handler.js
+++
@@ -1,8 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  if (res.headersSent) {
-    return next(err);
-  }
-  const status = err.status || 500;
-  const message = err.message || "Unexpected server error";
-  return res.status(status).json({ error: message });
-};
--- backend/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { register, login, refresh, logout } from "../controllers/auth-controller.js";
-
-export const authRouter = Router();
-
-authRouter.post("/register", register);
-authRouter.post("/login", login);
-authRouter.post("/refresh", refresh);
-authRouter.post("/logout", logout);
